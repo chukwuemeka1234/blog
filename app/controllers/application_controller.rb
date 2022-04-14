433459f35c3538ac95088c8c73138feebbe88dd3
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-04-14 20:48:50 +0100

    Add forms to project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(params[:user_id]) if params[:user_id]
   end
 end
