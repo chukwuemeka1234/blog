3f7d92052ef63e5e86670f2d36f217124ef79aad
Ricky Mormor <rickymormor1@gmail.com>
2022-04-23 22:49:27 +0100

    Add login and register methods to users_controller api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,4 +10,32 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: { success: true, data: { user: @user, message: 'user retrieved succeffully' } }
   end
+
+  def register
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    if @user.save
+      render json: { success: true, message: 'User was successfully created.' }, status: :ok
+    else
+      render json: { success: false, message: 'User was not created.' }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    valid = User.find_by(email: params[:email]).valid_password?(params[:password])
+    if valid
+      @user = User.find_by(email: params[:email])
+      @user.save
+      respond_to do |format|
+        format.json do
+          render json: { success: true, message: 'User was logged in.', user: @user }, status: :ok
+        end
+      end
+    else
+      respond_to do |format|
+        format.json do
+          render json: { success: false, message: 'User was not logged in.' }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
 end
